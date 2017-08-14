b6d31d6bdf74c003f4a43acac415f3367e8613ab
MasterUNG <phrombutr@gmail.com>
2017-08-14 13:01:39 +0700

    On Touch OK

--- app/src/main/java/app/ewtc/masterung/hellodji2/MainActivity.java
+++ app/src/main/java/app/ewtc/masterung/hellodji2/MainActivity.java
@@ -5,15 +5,49 @@ import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // When the compile and target version is higher than 22, please request the
-        // following permissions at runtime to ensure the
-        // SDK work well.
+
+        // Config Permission
+        configPermission();
+
+        setContentView(R.layout.activity_main);
+
+        //Mic Controller
+        micController();
+
+    }   // Main Method
+
+    private void micController() {
+        final ImageView imageView = (ImageView) findViewById(R.id.imvMic);
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    imageView.setImageResource(R.mipmap.ic_mic_on);
+                    //Process Partii Record
+
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    imageView.setImageResource(R.mipmap.ic_mic_off);
+                    //Stop Process Partii Record
+                }
+
+
+
+                return true;
+            }
+        });
+    }
+
+    private void configPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
@@ -25,8 +59,7 @@ public class MainActivity extends AppCompatActivity {
                             Manifest.permission.READ_PHONE_STATE,
                     }
                     , 1);
-        }
-
-        setContentView(R.layout.activity_main);
+        }   // if
     }
-}
+
+}   // Main Class
